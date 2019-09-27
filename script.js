8874b8137a71246118a8bee5c6eac3094e90e98e
elaintran <elain.ttran@gmail.com>
2019-09-27 16:40:12 -0500

    updated monitor according to new api

--- script.js
+++ script.js
@@ -1,95 +1,109 @@
 var streamers = document.querySelector(".streamers");
 
 var channels = [
-  "akariss",
-  "dire_thunder",
-  "disguisedtoast",
-  "joshjepson",
-  "nimious",
-  "marbelsoda",
-  "scarra",
-  "fedmyster",
-  "lilypichu",
-  "pokimane",
-  "xell",
-  "teegrassi",
-  "taj1ma",
-  "mechaf",
-  "shietz",
-  "vinsonte"
+    "akariss",
+    "dire_thunder",
+    "nimious",
+    "joshjepson",
+    "teegrassi",
+    "taj1ma",
+    "mechaf",
+    "shietz",
+    "vinsonte",
+    "doremy_puyotet",
+    "wumbotize",
+    "itsaruvn",
+    "etce",
+    "cpokemon12",
+    "cryaotic",
+    "sykkuno",
+    "kilimepie"
 ];
 
-var streamURL = 'https://api.twitch.tv/kraken/streams/';
-var streamChannel = 'https://api.twitch.tv/kraken/channels/';
-var callback = '?&client_id=iuxioif0oajo72w7a5kgbeuj7cppwp';
-
-getChannelInfo();
+var callback = "iuxioif0oajo72w7a5kgbeuj7cppwp";
 
 function getChannelInfo() {
-  channels.forEach(function(channel) {
-    function makeStreamURL(type, name) {
-      return "https://api.twitch.tv/kraken/" + type + "/" + name + callback;
-    }
-      $.getJSON(makeStreamURL("streams", channel), function(data) {
-        console.log(data);
-        if (data.stream === null) {
-          var status = "offline";
-          var game = "Offline";
-          var viewers = "";
-        } else {
-          var game = data.stream.game;
-          var live = data.stream.stream_type;
-          var status = "live";
-          var preview = data.stream.preview.large;
-          var viewers = data.stream.viewers;
-          //var preview = data.video_banner;
-        }
-        $.getJSON(makeStreamURL("channels", channel), function(channelData) {
-          if (game == "Offline") {
-            var newPreview = channelData.video_banner;
-            var gameLink = game;
-          } else {
-            var newPreview = preview;
-            var gameLink = "<a href ='https://www.twitch.tv/directory/game/" + game + "' class='game-link' target='_blank'>" + game + "</a>";
-          }
-          var html = "<div class='col-sm-4 " + status + "'><a href ='" + channelData.url +
-          "' target='_blank'><div class='box'><div class='circle'></div>" + status + "</div><img src='" + newPreview +
-          "' class='preview'><div class='viewers'><i class='fa fa-user'></i> " + viewers +
-          "</div></a><div class='streamers'><h4><a href ='" + channelData.url + "' target='_blank'>" + channelData.display_name +
-          "</a></h4><p>" + gameLink + "</p></div></div>";
-          if (status === "live") {
-            $(".row").prepend(html);
-          } else {
-            $(".row").append(html);
-          }
-          //console.log(channelData);
+    channels.forEach(function(channel) {
+        //gets basic user info from streamers
+        $.ajax({
+            method: "GET",
+            headers: {
+                "Client-Id": callback
+            },
+            url: makeStreamURL("users?login=", channel)
+        }).then(function(userData) {
+            var status = "";
+            var game = "";
+            var preview = userData.data[0].offline_image_url;
+            var gameLink = "";
+            //gets info from users currently streaming
+            $.ajax({
+                method: "GET",
+                headers: {
+                    "Client-ID": callback
+                },
+                url: makeStreamURL("streams?user_login=", channel)
+            }).then(function(streamData) {
+                if (streamData.data.length === 0) {
+                    status = "offline";
+                    game = "Offline";
+                } else {
+                    status = "live";
+                    preview = "https://static-cdn.jtvnw.net/previews-ttv/live_user_" + channel + "-1920x1080.jpg";
+                    return $.ajax({
+                        method: "GET",
+                        headers: {
+                            "Client-ID": callback
+                        },
+                        url: makeStreamURL("games?id=", streamData.data[0].game_id)
+                    }).then(function(gameName) {
+                        game = gameName.data[0].name;
+                    });
+                }
+            }).then(function() {
+                if (status === "offline" && preview === "") {
+                    preview = "images/black-background.png";
+                }
+                gameLink = "<a href ='https://www.twitch.tv/directory/game/" + game + "' class='game-link' target='_blank'>" + game + "</a>";
+                var html = "<div class='col-sm-4 " + status + "'><a href ='https://www.twitch.tv/" + channel +
+                "' target='_blank'><div class='box'><div class='circle'></div>" + status + "</div><img src='" + preview +
+                "' class='preview'></a><div class='streamers'><h4><a href ='https://www.twitch.tv/" + channel + "' target='_blank'>" + userData.data[0].display_name +
+                "</a></h4><p>" + gameLink + "</p></div></div>";
+                if (status === "live") {
+                    $(".row").prepend(html);
+                } else {
+                    $(".row").append(html);
+                }
+            });
         });
-      });
-  });
+    });
+}
+getChannelInfo();
+
+function makeStreamURL(type, name) {
+    return "https://api.twitch.tv/helix/" + type + name;
 }
 
 $(document).ready(function() {
-  $(".all-tag").on("click", function() {
-    $(".all-tag").addClass("active");
-    $(".live-tag").removeClass("active");
-    $(".offline-tag").removeClass("active");
-    $(".live").show();
-    $(".offline").show();
-  })
-  $(".live-tag").on("click", function() {
-    $(".live-tag").addClass("active");
-    $(".all-tag").removeClass("active");
-    $(".offline-tag").removeClass("active");
-    $(".live").show();
-    $(".offline").hide();
-  })
-  $(".offline-tag").on("click", function() {
-    $(".offline-tag").addClass("active");
-    $(".all-tag").removeClass("active");
-    $(".live-tag").removeClass("active");
-    $(".offline").show();
-    $(".live").hide();
-  })
+    $(".all-tag").on("click", function() {
+        $(".all-tag").addClass("active");
+        $(".live-tag").removeClass("active");
+        $(".offline-tag").removeClass("active");
+        $(".live").show();
+        $(".offline").show();
+    })
+    $(".live-tag").on("click", function() {
+        $(".live-tag").addClass("active");
+        $(".all-tag").removeClass("active");
+        $(".offline-tag").removeClass("active");
+        $(".live").show();
+        $(".offline").hide();
+    })
+    $(".offline-tag").on("click", function() {
+        $(".offline-tag").addClass("active");
+        $(".all-tag").removeClass("active");
+        $(".live-tag").removeClass("active");
+        $(".offline").show();
+        $(".live").hide();
+    })
 })
-
-//https://www.twitch.tv/directory/game/
